aa0fcc79d91de31a218dc219bcaebd50cd1613d4
Charles Schultz <cschultz@cox.net>
2018-06-25 20:21:34 -0700

    Removed unused import

--- src/app/card.service.ts
+++ src/app/card.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Card } from './card';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { CardCondition } from './condition';
 import { CardSingle } from './cardSingle';
